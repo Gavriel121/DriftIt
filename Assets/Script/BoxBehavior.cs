ff43feb0e19854a45aaf65aefa2f66ad811237ab
Gavriel121 <93674166+Gavriel121@users.noreply.github.com>
2022-04-10 16:55:22 +0300

    Initial commit

---
+++ Assets/Script/BoxBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehavior : MonoBehaviour
+{
+
+    public GameObject box;
+    public GameObject block;
+
+    public Transform[] spawnPoints;
+
+    public GameObject SpawnEffect;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            Destroy(gameObject);
+            Spawnbox();
+            SpawnBlock();
+            
+        }
+
+        void Spawnbox()
+        {
+            Instantiate(SpawnEffect, transform.position, SpawnEffect.transform.rotation);
+
+            int spawnPointX = Random.Range(-12, 12);
+            int spawnPointZ = Random.Range(-12, 12);
+            int spawnPointY = Random.Range(1, 1);
+
+            Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
+            Instantiate(box, spawnPosition, Quaternion.identity);
+        }
+
+        void SpawnBlock()
+        {
+            int spawnPointX = Random.Range(-12, 12);
+            int spawnPointZ = Random.Range(-12, 12);
+            float spawnPointY = Random.Range(10, 10);
+
+            Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
+            Instantiate(block, spawnPosition, Quaternion.identity);
+        }
+
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Joystick joystick;
+    public Joystick joystickTwo;
+
+    public float MoveSpeed = 100;
+    public float MaxSpeed = 20;
+    public float Drag = 0.98f;
+    public float SteerAngle = 20;
+    public float Traction = 1;
+
+    private Vector3 MoveForce;
+
+    public AudioSource audioSource;
+    public AudioClip PickUp;
+    public AudioClip Explosion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        /* MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+         transform.position += MoveForce * Time.deltaTime;*/
+
+        //Joystick movement
+        MoveForce += transform.forward * MoveSpeed * joystick.Vertical * Time.deltaTime;
+        transform.position += MoveForce * Time.deltaTime;
+
+        //Steer
+        /*float steerInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);*/
+
+        //Joystick steer
+        float steerInput = joystickTwo.Horizontal;
+        transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
+
+        //Drag + speed limit
+        MoveForce *= Drag;
+        MoveForce = Vector3.ClampMagnitude(MoveForce, MaxSpeed);
+
+        //Traction
+        MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveForce.magnitude;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Deathwall")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (other.tag == "Blocker")
+        {
+            Destroy(gameObject);
+
+            audioSource.PlayOneShot(Explosion);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (other.tag == "Box")
+        {
+            ScoreSystem.instance.Addpoint();
+
+            audioSource.PlayOneShot(PickUp);
+
+        }
+
+        if (other.tag == "Deathwall")
+        {
+
+            audioSource.PlayOneShot(Explosion);
+
+        }
+    }
+}
